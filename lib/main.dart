b5eca8b863b453133ad242f1765599d436b29869
WillCui- <wyfcui@gmail.com>
2021-02-13 12:29:06 -0500

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -13,7 +16,7 @@ class MyApp extends StatelessWidget {
       ),
       home: HomePage(),
     );
-}
+  }
 }
 
 class HomePage extends StatefulWidget {
@@ -22,33 +25,73 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-@override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "Home Page",
-        )
-      ),
-    body: Container(
-      alignment: Alignment.center,
-      color: Colors.teal[200],
-      child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Image(
-        image: AssetImage('assets/penguin.png'),
-        height: 200,
-        width: 200
-      ),
-        Text(
-        "We are in this together!",
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          fontSize: 20.0,
-          fontFamily: "Times New Roman",
-        ))
-      ])),
+          title: Text(
+        "Home Page",
+      )),
+      body: Container(
+          alignment: Alignment.center,
+          color: Colors.teal[200],
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image(
+                    image: AssetImage('assets/penguin.png'),
+                    height: 200,
+                    width: 200),
+                Text("We are in this together!",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontFamily: "Times New Roman",
+                    ))
+              ])),
     );
+  }
 }
+
+/* Sample Code
+class App extends StatefulWidget {
+  _AppState createState() => _AppState();
 }
+class _AppState extends State<App> {
+  // Set default `_initialized` and `_error` state to false
+  bool _initialized = false;
+  bool _error = false;
+  // Define an async function to initialize FlutterFire
+  void initializeFlutterFire() async {
+    try {
+      // Wait for Firebase to initialize and set `_initialized` state to true
+      await Firebase.initializeApp();
+      setState(() {
+        _initialized = true;
+      });
+    } catch(e) {
+      // Set `_error` state to true if Firebase initialization fails
+      setState(() {
+        _error = true;
+      });
+    }
+  }
+  @override
+  void initState() {
+    initializeFlutterFire();
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    // Show error message if initialization failed
+    if(_error) {
+      return SomethingWentWrong();
+    }
+    // Show a loader until FlutterFire is initialized
+    if (!_initialized) {
+      return Loading();
+    }
+    return MyAwesomeApp();
+  }
+}
+*/
